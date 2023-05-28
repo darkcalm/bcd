7a7b8976c1bfbced2d0fda8f440eedce65e0d4ec
darkcalm <132427921+darkcalm@users.noreply.github.com>
2023-05-28 06:14:45 +0000

    hash header

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from PIL import Image, ImageDraw, ImageFont
 from pilmoji import Pilmoji
 import emoji
 
-# hotfixed pil (font.size if hasattr(font,'size') else font.font.size)
+# hotfix pil? (font.size if hasattr(font,'size') else font.font.size)
 
 import os
 import openai
 import asyncio
+
+import re
+import math
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -21,11 +24,17 @@ PARAM_NUM = 11
 HASH_DELIM = '.:.'
 REPLY_DELIM = ','
 BREAK_DELIM = '\n'
-LINE_WIDTH = 1
-WIDEFIT = 1
-MIDFIT = 0.8
-BACKGROUND_COLOR = (255,255,255)
-FONT_COLOR = (0,0,0)
+FONT_SIZE_REGEX = r"^font (\d+)(,)*(.*)$"
+DELETION = ['delete', 'd']
+def reset_parameters():
+	global LINE_WIDTH, WIDEFIT, MIDFIT, BACKGROUND_COLOR, FONT_COLOR, FONT_SIZE
+	LINE_WIDTH = 1
+	WIDEFIT = 1
+	MIDFIT = 0.8
+	BACKGROUND_COLOR = (255,255,255)
+	FONT_COLOR = (0,0,0)
+	FONT_SIZE = 24
+reset_parameters()
 
 bot = commands.Bot(
 	command_prefix='/',
@@ -49,26 +58,254 @@ async def on_message(interaction):
 		target = await interaction.channel.fetch_message(
 			interaction.reference.message_id
 		)
-		if target.author.id == bot.user.id:
-			if interaction.content in ['delete', 'd']:
-				await target.delete()
+		if hasattr(target, 'author'):
+			if target.author.id == bot.user.id:
+				if interaction.content in DELETION:
+					await target.delete()
 
-			else:
-				amends = await tohash(interaction.content)
-				if amends == False:
-					await interaction.author.send(
-						"syntax: x label"+REPLY_DELIM+" y spaced label"
-					)
-				
 				else:
-					hash = await amendhash(target.content, amends)
-					await tofile(hash)
-					with open('diagram.png', 'rb') as f:
-						await interaction.channel.send(
-							hash,
-							file = discord.File(f)
+					amends = await tohash(interaction.content)
+					if amends == False:
+						await interaction.author.send(
+							"syntax: x label"+REPLY_DELIM+" y spaced label"
 						)
-							
+					
+					else:
+						hash = await amendhash(target.content, amends)
+						await tofile(hash)
+						with open('diagram.png', 'rb') as f:
+							await interaction.channel.send(
+								hash,
+								file = discord.File(f)
+							)	
+			else:
+				pass
+
+#### utility
+
+options = [
+	'_x','_y','_xn','_xp','_yn','_yp','_1','_2','_3','_4','_t',
+	'x','y','xn','xp','yn','yp','1','2','3','4','t'
+]
+
+
+async def tohash(hash):
+	if hash is None:
+		return None
+
+	match = re.match(FONT_SIZE_REGEX, hash)
+	if match:
+		global FONT_SIZE
+		FONT_SIZE = int(match.group(1))
+		hash = match.group(3) or HASH_DELIM*(PARAM_NUM-1)
+	
+	cuts = hash.split(REPLY_DELIM)
+	if len(cuts) == 1 and len(hash.split(HASH_DELIM)) > 1:
+		if len(hash.split((HASH_DELIM))) == PARAM_NUM:
+			return hash
+		return False
+		
+	processed = {}
+	for cut in cuts:
+		c = cut.strip(" ").split(" ")
+		if len(c) == 1 or c[0] not in options:
+			return False
+		else:
+			key = options.index(c[0]) % PARAM_NUM
+			if key in processed.keys():
+				processed[key] += BREAK_DELIM + " ".join(c[1:])
+			else:
+				processed[key] = " ".join(c[1:])
+
+	hash = ""
+
+	keys = list(processed.keys())
+	values = list(processed.values())
+	for i in range(PARAM_NUM):
+		if i in processed.keys():
+			hash += values[keys.index(i)]
+		if i != PARAM_NUM - 1:
+			hash += HASH_DELIM
+	
+	return hash
+
+
+async def amendhash(hash=None, hash2=None):
+	if hash2 == None:
+		return hash
+	if hash == None:
+		if isinstance(hash2, list):
+			return HASH_DELIM.join(hash2)
+		else:
+			return hash2
+	if not isinstance(hash2, list):
+		hash2 = hash2.split(HASH_DELIM)
+		
+	for i, keep in enumerate(hash.split(HASH_DELIM)):
+		if hash2[i] == "":
+			hash2[i] = keep
+	return HASH_DELIM.join(hash2)
+
+
+
+
+
+# big function, needs separation
+
+async def tofile(hash):
+	
+	prep = hash.split(HASH_DELIM)
+	preplen = len(prep)
+	if preplen < PARAM_NUM:
+		prep = prep.extend([""]*(PARAM_NUM-preplen))
+	elif preplen > PARAM_NUM:
+		prep = prep[:PARAM_NUM]
+	[_x, _y, _xn, _xp, _yn, _yp, _1, _2, _3, _4, _t] = prep
+	
+	width = 800
+	height = 800
+	ox = width/2
+	oy = height/2
+	q1x = ox + (width-ox)/2
+	q1y = oy - (height-oy)/2
+	q2x = ox - (width-ox)/2
+	q2y = q1y
+	q3x = q2x
+	q3y = oy + (height-oy)/2
+	q4x = q1x
+	q4y = q3y
+
+	
+	# images
+	image = Image.new('RGB', (width, height), color=BACKGROUND_COLOR)
+	
+	# draws
+	draw = ImageDraw.Draw(image)
+	
+	# fonts
+	font = ImageFont.truetype('OpenSansEmoji.ttf', FONT_SIZE)
+	font90 = ImageFont.TransposedFont(font, Image.ROTATE_90)
+
+	# utilities
+	def getw(target):
+		return font.getlength(target)
+
+	def geth(target):
+		return font.getbbox(target)[3]
+	
+	def drawtextdefault(xy, text, font=font):
+		with Pilmoji(image) as pilmoji:
+			pilmoji.text((int(xy[0]), int(xy[1])), text, font=font, fill=FONT_COLOR)
+
+	def drawprose(xy, text, wrapsize, anchor, font=font):
+		wrap = []
+		lineh = []
+		size = 0
+		lasti = 0
+
+		def appendwrap(text):
+			wrap.append(text)
+			lineh.append(geth(text))
+		
+		for i in range(len(text)):
+
+			if text[i] == '\n':
+				appendwrap(text[lasti:i].strip("\n").strip(" "))
+				appendwrap('\n')
+				lasti = i + 1
+				size = 0
+
+			elif size + getw(text[i]) < wrapsize:
+				size += getw(text[i])
+				
+			else:
+				if len(text[lasti:i].strip(" ").split(" ")) < 2:
+					appendwrap(text[lasti:i].strip(" "))
+					lasti = i
+				else:
+					append = " ".join(text[lasti:i].strip(" ").split(" ")[:-1])
+					appendwrap(append)
+					lasti = lasti + len(append) + 1
+				size = 0
+		appendwrap(text[lasti:].strip(" "))
+
+		totalh = sum(lineh)
+		x, y = {'h-': xy,
+						'h0': (xy[0], xy[1] - totalh/2),
+						'h+': (xy[0], xy[1] - totalh),
+						'v-': xy,
+						'v0': (xy[0] - totalh/2, xy[1]),
+						'v+': (xy[0] - totalh, xy[1])
+					 } [anchor[0] + anchor[2]]
+		
+		for i in range(len(wrap)):
+			drawtextdefault({
+				'h+': (x, y + sum(lineh[:i])),
+				'h0': (x - getw(wrap[i])/2, y + sum(lineh[:i])),
+				'h-': (x - getw(wrap[i]), y + sum(lineh[:i])),
+				'v+': (x + sum(lineh[:i]), y - getw(wrap[i])),
+				'v0': (x + sum(lineh[:i]), y - getw(wrap[i])/2),
+				'v-': (x + sum(lineh[:i]), y)
+				} [anchor[0] + anchor[1]], wrap[i], font=font)
+		pass
+		
+	def drawline(xyxy):
+		draw.line(xyxy, fill=FONT_COLOR)
+
+	def drawcurve(xyr):
+		r = int(math.sqrt((xyr[0]-xyr[2])**2 + (xyr[1]-xyr[3])**2))
+		xyxy = (xyr[0] - r, xyr[1] - r, xyr[0] + r, xyr[1] + r)
+		draw.ellipse(xyxy, outline=FONT_COLOR)
+
+
+	#should replace drawing functions with identification and draw after effecient shape is calculated
+
+	#objects
+	
+	drawprose((q1x, q1y), _1, (width-ox) * MIDFIT, 'h00')
+	drawprose((q2x, q2y), _2, (ox) * MIDFIT, 'h00')
+	drawprose((q3x, q3y), _3, (ox) * MIDFIT, 'h00')
+	drawprose((q4x, q4y), _4, (width-ox) * MIDFIT, 'h00')
+
+
+	#morphisms
+	
+	if (_xn != ''):
+		drawline((0, oy, ox - LINE_WIDTH/2, oy))
+		drawprose((0, oy), _xn, ox * MIDFIT, 'h++')
+
+	if (_xp != ''):
+		drawline((width, oy, ox - LINE_WIDTH/2, oy))
+		drawprose((width, oy), _xp, (width-ox) * MIDFIT, 'h-+')
+	
+	if (_yn != ''):
+		drawline((ox - LINE_WIDTH/2, height, ox - LINE_WIDTH/2, oy))
+		drawprose((ox, height), _yn, (width-ox) * MIDFIT, 'h++')
+	
+	if (_yp != ''):
+		drawline((ox - LINE_WIDTH/2, 0, ox - LINE_WIDTH/2, oy))
+		drawprose((ox, 0), _yp, (width-ox) * MIDFIT, 'h+-')	
+
+
+	#functors 
+	
+	drawprose((q1x, oy), _x, (width-ox) * WIDEFIT, 'h0-')
+	drawprose((ox, q1y), _y, (oy) * WIDEFIT, 'v0+', font90)
+	
+	
+	#most efficient shape
+
+	
+	# draw title
+	drawprose((0, 0), _t, ox * MIDFIT, 'h+-')
+	
+	image.save('diagram.png')
+
+	reset_parameters()
+
+
+# tbt
+
 @bot.tree.command(name='tbt')
 @app_commands.describe(
 	hash = "🗺️.:.⏱️.:.🏞️.:.🏬.:.🌜.:.🌞.:.🌇.:.🌄.:.🌌.:.🌃.:.🥱",
@@ -122,222 +359,22 @@ async def tbt(interaction:discord.Interaction,
 				)
 
 	except Exception as e:
-		await interaction.author.send(e)
-	
-
-#### utility
-
-options = [
-	'_x','_y','_xn','_xp','_yn','_yp','_1','_2','_3','_4','_t',
-	'x','y','xn','xp','yn','yp','1','2','3','4','t'
-]
-
-
-
-async def tohash(hash):
-	if hash is None:
-		return hash
-	
-	cuts = hash.split(REPLY_DELIM)
-	if len(cuts) == 1 and len(hash.split(HASH_DELIM)) > 1:
-		if len(hash.split((HASH_DELIM))) == PARAM_NUM:
-			return hash
-		return False
-		
-	processed = {}
-	for cut in cuts:
-		c = cut.strip(" ").split(" ")
-		if len(c) == 1 or c[0] not in options:
-			return False
+		if hasattr(interaction, 'author'):
+			await interaction.author.send(e)
 		else:
-			key = options.index(c[0]) % PARAM_NUM
-			if key in processed.keys():
-				processed[key] += BREAK_DELIM + " ".join(c[1:])
-			else:
-				processed[key] = " ".join(c[1:])
-
-	hash = ""
-
-	keys = list(processed.keys())
-	values = list(processed.values())
-	for i in range(PARAM_NUM):
-		if i in processed.keys():
-			hash += values[keys.index(i)]
-		if i != PARAM_NUM - 1:
-			hash += HASH_DELIM
-	
-	return hash
-
-
-async def amendhash(hash=None, hash2=None):
-	if hash2 == None:
-		return hash
-	if hash == None:
-		if isinstance(hash2, list):
-			return HASH_DELIM.join(hash2)
-		else:
-			return hash2
-	if not isinstance(hash2, list):
-		hash2 = hash2.split(HASH_DELIM)
-		
-	for i, keep in enumerate(hash.split(HASH_DELIM)):
-		if hash2[i] == "":
-			hash2[i] = keep
-	return HASH_DELIM.join(hash2)
-
-
-# big function, needs separation
-
-async def tofile(hash):
-	prep = hash.split(HASH_DELIM)
-	preplen = len(prep)
-	if preplen < PARAM_NUM:
-		prep = prep.extend([""]*(PARAM_NUM-preplen))
-	elif preplen > PARAM_NUM:
-		prep = prep[:PARAM_NUM]
-	[_x, _y, _xn, _xp, _yn, _yp, _1, _2, _3, _4, _t] = prep
-	
-	width = 800
-	height = 800
-	ox = width/2
-	oy = height/2
-	q1x = ox + (width-ox)/2
-	q1y = oy - (height-oy)/2
-	q2x = ox - (width-ox)/2
-	q2y = q1y
-	q3x = q2x
-	q3y = oy + (height-oy)/2
-	q4x = q1x
-	q4y = q3y
-
-	
-	# images
-	image = Image.new('RGB', (width, height), color=BACKGROUND_COLOR)
-	
-	# draws
-	draw = ImageDraw.Draw(image)
-	
-	# fonts
-	font = ImageFont.truetype('OpenSansEmoji.ttf', 24)
-	font90 = ImageFont.TransposedFont(font, Image.ROTATE_90)
-
-	# utilities
-	def getw(target):
-		return font.getlength(target)
-
-	def geth(target):
-		return font.getbbox(target)[3]
-	
-	def drawtextdefault(xy, text, font=font):
-		with Pilmoji(image) as pilmoji:
-			pilmoji.text((int(xy[0]), int(xy[1])), text, font=font, fill=FONT_COLOR)
-
-	def drawprose(xy, text, wrapsize, anchor, font=font):
-		wrap = []
-		size = 0
-		lasti = 0
-		for i in range(len(text)):
-
-			if text[i] == '\n':
-				wrap.append(text[lasti:i].strip("\n").strip(" "))
-				wrap.append('\n')
-				lasti = i + 1
-				size = 0
-
-			elif size + getw(text[i]) < wrapsize:
-				size += getw(text[i])
-				
-			else:
-				if len(text[lasti:i].strip(" ").split(" ")) < 2:
-					wrap.append(text[lasti:i].strip(" "))
-					lasti = i
-				else:
-					append = " ".join(text[lasti:i].strip(" ").split(" ")[:-1])
-					wrap.append(append)
-					lasti = lasti + len(append) + 1
-				size = 0
-		wrap.append(text[lasti:].strip(" "))
-		
-		lineh = geth(text)
-		totalh = lineh * len(wrap)
-		x, y = {'h-': xy,
-						'h0': (xy[0], xy[1] - totalh/2),
-						'h+': (xy[0], xy[1] - totalh),
-						'v-': xy,
-						'v0': (xy[0] - totalh/2, xy[1]),
-						'v+': (xy[0] - totalh, xy[1])
-					 } [anchor[0] + anchor[2]]
-		for i in range(len(wrap)):
-			drawtextdefault({
-				'h+': (x, y + lineh*i),
-				'h0': (x - getw(wrap[i])/2, y + lineh*i),
-				'h-': (x - getw(wrap[i]), y + lineh*i),
-				'v+': (x + lineh*i, y - getw(wrap[i])),
-				'v0': (x + lineh*i, y - getw(wrap[i])/2),
-				'v-': (x + lineh*i, y)
-				} [anchor[0] + anchor[1]], wrap[i], font=font)
-		pass
-		
-	def drawline(xy):
-		draw.line(xy, fill=FONT_COLOR)
-
-
-	#should replace drawing functions with identification and draw after effecient shape is calculated
-
-	#objects
-	
-	drawprose((q1x, q1y), _1, (width-ox) * MIDFIT, 'h00')
-	drawprose((q2x, q2y), _2, (ox) * MIDFIT, 'h00')
-	drawprose((q3x, q3y), _3, (ox) * MIDFIT, 'h00')
-	drawprose((q4x, q4y), _4, (width-ox) * MIDFIT, 'h00')
-
-
-	#morphisms
-	
-	if (_xn != ''):
-		drawline((0, oy, ox - LINE_WIDTH/2, oy))
-		drawprose((0, oy), _xn, ox * MIDFIT, 'h++')
-
-	if (_xp != ''):
-		drawline((ox - LINE_WIDTH/2, oy, width, oy))
-		drawprose((width, oy), _xp, (width-ox) * MIDFIT, 'h-+')
-	
-	if (_yn != ''):
-		drawline((ox - LINE_WIDTH/2, height, ox - LINE_WIDTH/2, oy))
-		drawprose((ox, height), _yn, (width-ox) * MIDFIT, 'h++')
-	
-	if (_yp != ''):
-		drawline((ox - LINE_WIDTH/2, oy, ox - LINE_WIDTH/2, 0))
-		drawprose((ox, 0), _yp, (width-ox) * MIDFIT, 'h+-')	
-
-
-	#functors 
-	
-	drawprose((q1x, oy), _x, (width-ox) * WIDEFIT, 'h0-')
-	drawprose((ox, q1y), _y, (oy) * WIDEFIT, 'v0+', font90)
-	
-	
-	#most efficient shape
-
-	
-
-	# draw title
-	drawprose((0, 0), _t, ox * MIDFIT, 'h+-')
-	
-	# save image
-	image.save('diagram.png')
-
-
+			await interaction.user.send(e)
 
 
 # metaphor layer in development
 
 @bot.tree.command(name='ml2')
 @app_commands.describe(
-	_pub = "True = publish, otherwise by default only you can see the bot reply"
+	_theme = "basic query parameter",
+	_pub = "under developement"
 )
 async def ml2(interaction:discord.Interaction,
-						 _pub:bool=False):
+							_theme:str="loosely related concepts",
+							_pub:bool=False):
 	try:
 
 		await interaction.response.defer(ephemeral = not _pub)
@@ -346,7 +383,7 @@ async def ml2(interaction:discord.Interaction,
 		response = openai.ChatCompletion.create(
 	  	model="gpt-3.5-turbo", # 32K context gpt-4 model: "gpt-4-32k"
 	  	messages=[
-	      {"role": "system", "content": "11 random emojis to fill a 2x2 diagram (4 quadrant labels, 4 axis extremes labels, 2 axis labels, 1 title). Respond with 11 emojis only."}
+	      {"role": "system", "content": "11 emojis to fill a 2x2 diagram (4 quadrant labels, 4 axis extremes labels, 2 axis labels, 1 title) with the theme of: " + _theme + ". Respond with 11 emojis only."}
 	    ]
 		)
 
@@ -372,9 +409,24 @@ async def ml2(interaction:discord.Interaction,
 		await interaction.followup.send(e)
 	
 
-# bottom
 
-bot.run(TOKEN)
+# topology layer under development
+
+@bot.tree.command(name='topos')
+@app_commands.describe(
+	_pub = "under developement"
+)
+async def topos(interaction:discord.Interaction,
+						 _pub:bool=False):
+	try:
+		pass
+	
+	
+	except Exception as e:
+		if hasattr(interaction, 'author'):
+			await interaction.author.send(e)
+		else:
+			await interaction.user.send(e)
 
 
 
@@ -412,3 +464,4 @@ def draw_graph(L, M, N):
 
 
 
+bot.run(TOKEN)
